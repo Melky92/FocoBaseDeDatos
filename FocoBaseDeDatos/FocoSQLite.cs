940c26ed35407ca9746a178203fecd6b695da1f8
Melky <melky92@hotmail.com>
2017-02-01 11:07:02 -0400

    inicial: ejecutar comando y ejecutar lectura

---
+++ FocoBaseDeDatos/FocoSQLite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FocoBaseDeDatos
+{
+    class FocoSQLite
+    {
+        private String miCadenaConexion;
+        public FocoSQLite(String cadenaConexion)
+        {
+            //miCadenaConexion = @"Data Source=" + dirDB + ";Version=3;";
+            miCadenaConexion = cadenaConexion;
+        }
+        public void EjecutarComando(String comando)
+        {
+            using (SQLiteConnection miConexion = new SQLiteConnection(miCadenaConexion))
+            {
+                miConexion.Open();
+                if (miConexion.State == System.Data.ConnectionState.Open)
+                {
+                    SQLiteCommand miComando = new SQLiteCommand(comando, miConexion);
+                    miComando.ExecuteNonQuery();
+                    miConexion.Close();
+                }
+            }
+        }
+        public List<String> EjecutarLectura(String comando)
+        {
+            using (SQLiteConnection miConexion = new SQLiteConnection(miCadenaConexion))
+            {
+                miConexion.Open();
+                List<String> miLista = new List<String>();
+                if (miConexion.State == System.Data.ConnectionState.Open)
+                {
+                    SQLiteCommand miComando = new SQLiteCommand(comando, miConexion);
+                    SQLiteDataReader miLector = miComando.ExecuteReader();
+                    while (miLector.Read())
+                    {
+                        miLista.Add(miLector.ToString());
+                    }
+                    miConexion.Close();
+                }
+                return miLista;
+            }
+        }
+    }
+}
